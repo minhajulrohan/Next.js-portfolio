4a74d1dc10628019a33b559e90377c4d619f6275
Minhajul Rohan <125715530+minhajulrohan@users.noreply.github.com>
2024-05-28 16:03:04 +0600

    Update State.jsx

--- components/State.jsx
+++ components/State.jsx
@@ -26,7 +26,7 @@ const State = () => {
       <div className="container mx-auto">
       <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
       {stats.map((item, index) => {
-        return <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
+        return <div className="flex-1 flex gap-6 items-center justify-center xl:justify-start" key={index}>
           <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-semibold" />
           <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"}  leading-snug "text-white/80"`}>{item.text}</p>
         </div>
