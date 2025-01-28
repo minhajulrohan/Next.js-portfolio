4104b5995584248874d0ac6b03e7d443931cb0d3
Minhajul Rohan <125715530+minhajulrohan@users.noreply.github.com>
2025-01-28 12:24:49 +0600

    Create Document.jsx

---
+++ app/Document.jsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google AdSense Meta Tag */}
+        <meta name="google-adsense-account" content="ca-pub-1301444160765191" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
